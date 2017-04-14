4f388e870905af4a87399c490e1c68fad93dbc88
Eric Salerno <eric.salerno@viacom.com>
2017-04-14 15:52:12 -0400

    Add TDD tests

--- tests/FormatterTest.php
+++ tests/FormatterTest.php
@@ -10,5 +10,18 @@ namespace SalernoLabs\Tests\RelativeTime;
 
 class FormatterTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * Test relative time generation
+     * @covers \SalernoLabs\RelativeTime\Formatter::getRelativeTime
+     */
+    public function testRelativeTime()
+    {
+        $formatter = new \SalernoLabs\RelativeTime\Formatter();
 
+        $this->assertEquals('1 day ago', $formatter->getRelativeTime(new \DateTime('-1 day')));
+        $this->assertEquals('2 days ago', $formatter->getRelativeTime(new \DateTime('-2 days')));
+        $this->assertEquals('1 year ago', $formatter->getRelativeTime(new \DateTime('-1 year')));
+        $this->assertEquals('1 day from now', $formatter->getRelativeTime(new \DateTime('+1 day')));
+        $this->assertEquals('20 minutes ago', $formatter->getRelativeTime(new \DateTime('-20 minutes')));
+    }
 }
