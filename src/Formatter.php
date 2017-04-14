d2640c178321d54c500fc967e29181c8ce6d7cb3
Eric Salerno <eric.salerno@viacom.com>
2017-04-14 16:44:26 -0400

    Fixed Formatter class, adds a bunch of tests for things

--- src/Formatter.php
+++ src/Formatter.php
@@ -10,5 +10,161 @@ namespace SalernoLabs\RelativeTime;
 
 class Formatter
 {
+    /**
+     * @var \DateTime
+     */
+    private $startDate;
 
+    /**
+     * @var bool
+     */
+    private $showLabel = true;
+
+    /**
+     * Set start date
+     *
+     * @param \DateTime $startDate
+     * @return $this
+     */
+    public function setStartDate(\DateTime $startDate)
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    /**
+     * Set show labels or not
+     *
+     * @param $showLabels
+     * @return $this
+     */
+    public function setShowLabels($showLabels)
+    {
+        $this->showLabel = $showLabels;
+
+        return $this;
+    }
+
+    /**
+     * Get the relative time as a string
+     *
+     * @param \DateTime $time
+     * @return string
+     */
+    public function getRelativeTime(\DateTime $time)
+    {
+        //Calculate the difference between the start and end dates
+        if (!empty($this->startDate))
+        {
+            $startingTimeStamp = intval($this->startDate->format('U'));
+        }
+        else
+        {
+            $startingTimeStamp = time();
+        }
+
+        $endingTimeStamp = intval($time->format('U'));
+
+        $difference = ($startingTimeStamp - $endingTimeStamp);
+
+        //First figure out the correct label to use
+        $label = '';
+        $isPast = ($difference > 0);
+
+        if ($this->showLabel)
+        {
+            $label = ($isPast ? ' ago' : ' from now');
+        }
+
+        //Use absolute value for figuring out the actual difference
+        $difference = abs($difference);
+
+        $minute = 60;
+        $hour = $minute * 60;
+        $day = $hour * 24;
+        $year = $day * 365;
+
+        $output = '';
+
+        //Get the right timeslice for the difference (code we've seen countless other places)
+        if ($difference < 10)
+        {
+            //It just happened, lets call it "just now"
+            if ($isPast)
+            {
+                $output .= "just now";
+            }
+            else
+            {
+                $output .= "momentarily";
+            }
+        }
+        else if ($difference < $minute)
+        {
+            //Its less than a minute but more than "just now" so lets give seconds.
+            $output .= $difference . " seconds" . $label;
+        }
+        else if ($difference < ($minute * 2))
+        {
+            //Less than two minutes so lets say "about a minute"
+            $output .= "about a minute" . $label;
+        }
+        else if ($difference < $hour)
+        {
+            //More than "about a minute" but less than an hour so lets say how many minutes
+            $amount = round($difference / $minute);
+            $output .= $amount . ' minute' . ($amount != 1 ? 's' : '') . $label;
+        }
+        else if ($difference < ($hour * 2))
+        {
+            //More than an hour but less than 2, lets say "about an hour"
+            $output .= "about an hour" . $label;
+        }
+        else if ($difference < $day)
+        {
+            //More than 2 hours but less than a day, lets give hours
+            $amount = round($difference / $hour);
+            $output .= $amount . ' hour' . ($amount != 1 ? 's' : '') . $label;
+        }
+        else if ($difference < ($day * 2))
+        {
+            //More than a day but less than 2, lets use human readable "yesterday" or "tomorrow"'s
+            if ($isPast)
+            {
+                $output .= "yesterday";
+            }
+            else
+            {
+                $output .= "tomorrow";
+            }
+        }
+        else if ($difference < $year)
+        {
+            //More than 2 days but less than a year, lets give number of days
+            $amount = round($difference / $day);
+            $output .= $amount . ' day' . ($amount != 1 ? 's' : '') . $label;
+        }
+        else
+        {
+            //More than a year, lets just give years
+            $amount = round($difference / $year);
+            $output .= $amount . ' year' . ($amount != 1 ? 's' : '') . $label;
+        }
+
+        return $output;
+    }
+
+    /**
+     * Get relative time as an HTML5 tag
+     *
+     * @param \DateTime $time
+     * @return string
+     */
+    public function getRelativeTimeTag(\DateTime $time)
+    {
+        $timeString = $this->getRelativeTime($time);
+
+        return '<time title="' . $time->format('F jS, Y - g:iA T') . '">' . $timeString . '</time>';
+    }
 }
--- tests/FormatterTest.php
+++ tests/FormatterTest.php
@@ -13,15 +13,111 @@ class FormatterTest extends \PHPUnit\Framework\TestCase
     /**
      * Test relative time generation
      * @covers \SalernoLabs\RelativeTime\Formatter::getRelativeTime
+     * @dataProvider dataProviderForRelativeTimeTest
      */
-    public function testRelativeTime()
+    public function testRelativeTime($timeModify, $expectedOutput)
     {
         $formatter = new \SalernoLabs\RelativeTime\Formatter();
 
-        $this->assertEquals('1 day ago', $formatter->getRelativeTime(new \DateTime('-1 day')));
-        $this->assertEquals('2 days ago', $formatter->getRelativeTime(new \DateTime('-2 days')));
-        $this->assertEquals('1 year ago', $formatter->getRelativeTime(new \DateTime('-1 year')));
-        $this->assertEquals('1 day from now', $formatter->getRelativeTime(new \DateTime('+1 day')));
-        $this->assertEquals('20 minutes ago', $formatter->getRelativeTime(new \DateTime('-20 minutes')));
+        $this->assertEquals($expectedOutput, $formatter->getRelativeTime(new \DateTime($timeModify)));
     }
+
+    /**
+     * Data provider for the test
+     *
+     * @return array
+     */
+    public function dataProviderForRelativeTimeTest()
+    {
+        return [
+            ['-5 seconds', 'just now'],
+            ['+5 seconds', 'momentarily'],
+
+            ['-1 minutes', 'about a minute ago'],
+            ['+1 minutes', 'about a minute from now'],
+            ['-2 minutes', '2 minutes ago'],
+            ['+2 minutes', '2 minutes from now'],
+            ['-20 minutes', '20 minutes ago'],
+
+            ['-1 hour', 'about an hour ago'],
+            ['+1 hour', 'about an hour from now'],
+            ['-2 hours', '2 hours ago'],
+            ['+2 hours', '2 hours from now'],
+
+            ['-1 day', 'yesterday'],
+            ['-2 days', '2 days ago'],
+            ['+1 day', 'tomorrow'],
+            ['+2 days', '2 days from now'],
+
+            ['-1 year', '1 year ago'],
+            ['-2 year', '2 years ago'],
+            ['+1 year', '1 year from now'],
+            ['+2 years', '2 years from now']
+        ];
+    }
+
+    /**
+     * Test relative time tag generation
+     *
+     * @param $timeModify
+     * @param $expectedOutput
+     * @covers \SalernoLabs\RelativeTime\Formatter::getRelativeTimeTag
+     * @dataProvider dataProviderTimeTagTest
+     */
+    public function testRelativeTimeTag($timeModify, $expectedOutput)
+    {
+        $formatter = new \SalernoLabs\RelativeTime\Formatter();
+
+        $this->assertRegExp($expectedOutput, $formatter->getRelativeTimeTag(new \DateTime($timeModify)));
+    }
+
+    /**
+     * Test the generation of the time tag wtih this data
+     *
+     * @return array
+     */
+    public function dataProviderTimeTagTest()
+    {
+        return [
+            ['-1 day', '#<time title=".*?">yesterday</time>#'],
+            ['-2 day', '#<time title=".*?">2 days ago</time>#'],
+            ['-1 year', '#<time title=".*?">1 year ago</time>#'],
+            ['+5 seconds', '#<time title=".*?">momentarily</time>#'],
+        ];
+    }
+
+    /**
+     * Test no labels
+     *
+     * @param $timeModify
+     * @param $expectedOutput
+     * @covers \SalernoLabs\RelativeTime\Formatter::setShowLabels
+     * @dataProvider dataProviderNoLabelTest
+     */
+    public function testNoLabels($timeModify, $expectedOutput)
+    {
+        $formatter = new \SalernoLabs\RelativeTime\Formatter();
+
+        $formatter->setShowLabels(false);
+
+        $this->assertRegExp($expectedOutput, $formatter->getRelativeTimeTag(new \DateTime($timeModify)));
+    }
+
+    /**
+     * Data provider for no label test
+     *
+     * @return array
+     */
+    public function dataProviderNoLabelTest()
+    {
+        return [
+            ['-1 day', '#<time title=".*?">yesterday</time>#'],
+            ['+1 day', '#<time title=".*?">tomorrow</time>#'],
+            ['-2 day', '#<time title=".*?">2 days</time>#'],
+            ['-1 year', '#<time title=".*?">1 year</time>#'],
+            ['+5 seconds', '#<time title=".*?">momentarily</time>#'],
+            ['-2 year', '#<time title=".*?">2 years</time>#'],
+        ];
+    }
+
 }
