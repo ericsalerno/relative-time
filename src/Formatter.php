5d19f46b5d70ed7c8de095ab1fd96d27d4e3f87e
Eric Salerno <eric.salerno@viacom.com>
2017-04-14 15:23:16 -0400

    Class stubbing

---
+++ src/Formatter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Build a relative time string from a timestamp
+ *
+ * @package SalernoLabs
+ * @subpackage RelativeTime
+ * @author Eric Salerno
+ */
+namespace SalernoLabs\RelativeTime;
+
+class Formatter
+{
+
+}
---
+++ tests/FormatterTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Relative Time Test
+ *
+ * @package SalernoLabs
+ * @subpackage Tests
+ * @author Eric Salerno
+ */
+namespace SalernoLabs\Tests\RelativeTime;
+
+class FormatterTest extends \PHPUnit\Framework\TestCase
+{
+
+}
